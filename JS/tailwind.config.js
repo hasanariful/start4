13e7c19ada95563eb18067728e6c23492f397fae
hasanariful <arifulhasan90r@gmail.com>
2023-10-23 12:33:42 +0600

    first commit

---
+++ JS/tailwind.config.js
@@ -0,0 +1,45 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        // My code start here
+        // Color code start
+        colors: {
+          headercolor: 'rgba(22, 12, 109, 0.50)',
+          hovercolor: '#1BBF00',
+          overlycolor: 'rgba(106, 98, 197, 0.90)',
+        },
+        // Color code end
+        
+        //Background image part
+        backgroundImage: {
+          bannerimg: "url('Images/Bannerimg.jpg')",
+          counterimg: "url('Images/Counter.png')",
+          
+        },
+        //Background image part
+
+        //Padding Part 
+        
+        //Padding Part 
+
+        //Font Family
+        fontFamily: {
+          open: ['Open Sans', 'sans-serif'],
+          papri: ['Paprika ', 'cursive'],
+        },
+        //Font Family
+
+        //All Containers
+        width: {
+          headercontainer: '1170px',
+          bannertextcontainer: '962px',
+         },
+        //All Containers
+
+
+
+        // My code end here
+
+      }
+    }
+  }
